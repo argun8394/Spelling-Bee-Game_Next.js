b582ffdef1ccd89f71f9d0b68e0517867eb09ebd
argun8394 <argundoganali@gmail.com>
2024-05-23 18:17:52 +0300

    up

--- src/app/[locale]/page.jsx
+++ src/app/[locale]/page.jsx
@@ -1,5 +1,13 @@
+'use client'
 import Game from '@/components/game/Game';
+import { useParams } from 'next/navigation'
+
 export default function Home() {
+
+  const { locale } = useParams()
+
+  // console.log('router', locale)
+
   return (
     <main className="flex flex-col items-center justify-between p-24">
       < Game />
